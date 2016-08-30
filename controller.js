f5336d14cdf0654ddb60039e3b5e814a9c6a7730
Krister <krister.torsvik@gmail.com>
2016-08-30 12:55:11 +0200

    cleanup code and make button removebyid

--- controller.js
+++ controller.js
@@ -2,18 +2,29 @@ function initializeApp(listOfTodos) {
   createAddTodoForm();
   buildAddButton();
   for (var i = 0; i < listOfTodos.length; i++) {
-    var newDiv = createTodoListItem();
-    newDiv.innerHTML = createTodoListItem(listOfTodos[i].title, listOfTodos[i].description);
-    document.body.appendChild(newDiv);
-    var deleteButton = buildDeleteButton();
-    newDiv.appendChild(deleteButton);
+    //var newDiv = createTodoListItem();
+    createTodoList(listOfTodos[i].title, listOfTodos[i].description);
+    //newDiv.id = "divTodo" + i;
+    //document.body.appendChild(newDiv);
+    // var deleteButton = buildDeleteButton();
+    // deleteButton.addEventListener("click",() => document.body.removeChild(newDiv));
+    // newDiv.appendChild(deleteButton);
   }
+}
+function createTodoList(title, description) {
+  var newDiv = createTodoListItem();
+  var todo = { title: title, description: description };
+  var deleteButton = buildDeleteButton();
+  console.log(listOfTodos);
+  newDiv.innerHTML = todo.title + "<br>" + "<br>" + "<br>" + todo.description;
+  document.body.appendChild(newDiv);
+  deleteButton.addEventListener("click",() => document.body.removeChild(newDiv));
+  newDiv.appendChild(deleteButton);
 }
 
 function createAddTodoForm() {
   var form = buildForm();
   document.body.appendChild(form);
-
 }
 
 function addTodo() {
@@ -32,9 +43,9 @@ function addTodo() {
     createTodoList(input1.value, input2.value)
     document.body.appendChild(newDiv);
     var deleteButton = buildDeleteButton();
+    deleteButton.addEventListener("click",() => document.body.removeChild(newDiv));
     newDiv.appendChild(deleteButton);
     input1.value = "";
     input2.value = "";
   }
 }
-// function removeTodo(id) {deleteButton.addEventListener("click",() => document.body.removeChild(newDiv));}
