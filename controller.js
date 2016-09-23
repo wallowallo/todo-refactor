1cb012ab7a2cd3ff7ae6af96e0b8302b0040504c
Krister <krister.torsvik@gmail.com>
2016-09-23 16:01:11 +0200

    set id in for loop

--- controller.js
+++ controller.js
@@ -1,25 +1,13 @@
+createAddTodoForm();
+buildAddButton();
+
 function initializeApp(listOfTodos) {
-  createAddTodoForm();
-  buildAddButton();
-  for (var i = 0; i < listOfTodos.length; i++) {
-    //var newDiv = createTodoListItem();
-    createTodoList(listOfTodos[i].title, listOfTodos[i].description);
-    //newDiv.id = "divTodo" + i;
-    //document.body.appendChild(newDiv);
-    // var deleteButton = buildDeleteButton();
-    // deleteButton.addEventListener("click",() => document.body.removeChild(newDiv));
-    // newDiv.appendChild(deleteButton);
-  }
-}
-function createTodoList(title, description) {
-  var newDiv = createTodoListItem();
-  var todo = { title: title, description: description };
-  var deleteButton = buildDeleteButton();
-  console.log(listOfTodos);
-  newDiv.innerHTML = todo.title + "<br>" + "<br>" + "<br>" + todo.description;
-  document.body.appendChild(newDiv);
-  deleteButton.addEventListener("click",() => document.body.removeChild(newDiv));
-  newDiv.appendChild(deleteButton);
+   var newDiv = document.querySelector("todoList");
+   for (var i = 0; i < listOfTodos.length; i++) {
+     createTodoList(listOfTodos[i].title, listOfTodos[i].description);
+     newDiv.id = "divTodo" + i;
+   }
+   console.log(listOfTodos);
 }
 
 function createAddTodoForm() {
@@ -37,15 +25,21 @@ function addTodo() {
     function createTodoList(title, description) {
       var todo = { title: title, description: description };
       listOfTodos.push(todo);
-      console.log(listOfTodos);
       newDiv.innerHTML = todo.title + "<br>" + "<br>" + "<br>" + todo.description;
     }
     createTodoList(input1.value, input2.value)
-    document.body.appendChild(newDiv);
-    var deleteButton = buildDeleteButton();
-    deleteButton.addEventListener("click",() => document.body.removeChild(newDiv));
-    newDiv.appendChild(deleteButton);
     input1.value = "";
     input2.value = "";
   }
+  initializeApp(listOfTodos);
+}
+
+function createTodoList(title, description) {
+  var newDiv = createTodoListItem();
+  var todo = { title: title, description: description };
+  var deleteButton = buildDeleteButton();
+  newDiv.innerHTML = todo.title + "<br>" + "<br>" + "<br>" + todo.description;
+  document.body.appendChild(newDiv);
+  deleteButton.addEventListener("click",() => document.body.removeChild(newDiv));
+  newDiv.appendChild(deleteButton);
 }
